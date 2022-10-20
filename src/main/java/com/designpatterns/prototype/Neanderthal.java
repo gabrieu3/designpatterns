e033cd2cd2a6fa272a20b15544965365140ae720
Gabriel Balestrin <gabrieu3@gmail.com>
2022-10-20 15:17:22 -0300

    prototype pattern

---
+++ src/main/java/com/designpatterns/prototype/Neanderthal.java
@@ -0,0 +1,63 @@
+package com.designpatterns.prototype;
+
+import java.util.Date;
+
+public class Neanderthal implements Prototype{
+
+    private String fullName;
+    private Date birthday;
+    private Double weight;
+    private Double height;
+
+    public Neanderthal(String fullName, Date birthday, Double weight, Double height) {
+        this.fullName = fullName;
+        this.birthday = birthday;
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public Neanderthal() {
+    }
+
+    @Override
+    public Prototype clone() {
+        Neanderthal neanderthal = new Neanderthal();
+        neanderthal.setBirthday(this.birthday);
+        neanderthal.setFullName(this.fullName);
+        neanderthal.setWeight(this.weight);
+        neanderthal.setHeight(this.height);
+        return neanderthal;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+}
---
+++ src/main/java/com/designpatterns/prototype/Sapiens.java
@@ -0,0 +1,63 @@
+package com.designpatterns.prototype;
+
+import java.util.Date;
+
+public class Sapiens implements Prototype{
+
+    private String fullName;
+    private Date birthday;
+    private Double weight;
+    private Double height;
+
+    @Override
+    public Prototype clone() {
+        Sapiens sapien = new Sapiens();
+        sapien.setBirthday(this.birthday);
+        sapien.setFullName(this.fullName);
+        sapien.setWeight(this.weight);
+        sapien.setHeight(this.height);
+        return sapien;
+    }
+
+    public Sapiens(String fullName, Date birthday, Double weight, Double height) {
+        this.fullName = fullName;
+        this.birthday = birthday;
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public Sapiens() {
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+}
