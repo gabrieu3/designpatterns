522a0d2ab04cd2ab10e55c1a2210521eba0e954a
Gabriel Balestrin <gabrieu3@gmail.com>
2022-10-20 18:51:10 -0300

    factory pattern

---
+++ src/main/java/com/designpatterns/factory/ProductFactory.java
@@ -0,0 +1,8 @@
+package com.designpatterns.factory;
+
+import com.designpatterns.factory.products.Product;
+
+public abstract class ProductFactory {
+
+    public abstract Product getProduct();
+}
---
+++ src/main/java/com/designpatterns/factory/products/Magazine.java
@@ -0,0 +1,13 @@
+package com.designpatterns.factory.products;
+
+public class Magazine implements Product {
+    private Integer ID;
+    private String name;
+    private String description;
+    private Double price;
+
+    @Override
+    public Double getPromotionalValue() {
+        return price - price * 0.1;
+    }
+}
