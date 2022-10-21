d084e139b056812710e096cec5bf5ec0b08820f9
Gabriel Balestrin <gabrieu3@gmail.com>
2022-10-21 11:38:30 -0300

    open api

--- src/main/java/com/designpatterns/DesignpatternsApplication.java
+++ src/main/java/com/designpatterns/DesignpatternsApplication.java
@@ -5,6 +5,8 @@ import com.designpatterns.builder.withbuilder.Address;
 import com.designpatterns.builder.withbuilder.Client;
 import com.designpatterns.builder.withbuilder.Name;
 import com.designpatterns.singleton.ApplicationPropertiesSingleton;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,32 +14,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Design Pattern Api", version = "1.0", description = "Learn is so beautiful"))
 public class DesignpatternsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DesignpatternsApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(DesignpatternsApplication.class, args);
 
-		/**
-		 * SINGLETON
-		 */
-		ApplicationPropertiesSingleton app = ApplicationPropertiesSingleton.getInstance();
-		System.out.println(app.getProperty("author"));
+        /**
+         * SINGLETON
+         */
+        ApplicationPropertiesSingleton app = ApplicationPropertiesSingleton.getInstance();
+        System.out.println(app.getProperty("author"));
 
-		/**
-		 * BUILDER
-		 */
-		List<String> middleNames = new ArrayList<>();
-		middleNames.add("Meio1");
-		middleNames.add("Meio2");
-		Name name = Name.builder().firstName("Gabriel")
-								  .middleNames(middleNames)
-								  .lastName("Balestrin")
-								  .build();
-		Address address = Address.builder().address("Rua das Flores").build();
+        /**
+         * BUILDER
+         */
+        List<String> middleNames = new ArrayList<>();
+        middleNames.add("Meio1");
+        middleNames.add("Meio2");
+        Name name = Name.builder().firstName("Gabriel")
+                .middleNames(middleNames)
+                .lastName("Balestrin")
+                .build();
+        Address address = Address.builder().address("Rua das Flores").build();
 
-		Client client = Client.builder().name(name).address(address).email("gabriel@email.com").build();
+        Client client = Client.builder().name(name).address(address).email("gabriel@email.com").build();
 
 
-	}
+    }
 
 }
--- src/main/java/com/designpatterns/api/ApiDesignPatterns.java
+++ src/main/java/com/designpatterns/api/ApiDesignPatterns.java
@@ -1,8 +1,10 @@
 package com.designpatterns.api;
 
-import org.springframework.http.ResponseEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,18 +14,32 @@ import java.util.Map;
 @RestController
 public class ApiDesignPatterns {
 
-    @GetMapping(path="version")
+    @Operation(summary = "Application Verion")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful Operation",
+                    content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", description = "Invalid parameters supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Error",
+                    content = @Content)})
+    @GetMapping(path = "version")
     @ResponseBody
-    public Map<String,String> version(){
+    public Map<String, String> version() {
         Map<String, String> response = new HashMap<>();
-        response.put("version","1.0");
-        response.put("message","design patterns by gabrieu3");
+        response.put("version", "1.0");
+        response.put("message", "design patterns by gabrieu3");
         return response;
     }
 
+    @Operation(summary = "Application Home")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful Operation",
+                    content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", description = "Invalid parameters supplied",
+                    content = @Content)})
     @GetMapping
     @ResponseBody
-    public String home(){
+    public String home() {
         return "Hello Human!";
     }
 }
